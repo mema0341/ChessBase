c8c7fc55879916e1a0de35e1244b30f2e56bb7fc
meliss <mema0341@colorado.edu>
2021-01-20 19:51:26 -0700

    started adding move functionality

--- chess_playground.py
+++ chess_playground.py
@@ -1,8 +1,45 @@
+import numpy as np 
+
 class Piece:
-    def __init__(self, name, color,starting_loc):
+    def __init__(self, name, color,position):
         self.name = name
         self.color = color
-        self.starting_loc = starting_loc
+        self.position = position
+
+        # if name == "pawn":
+        #     print("Pawn Created")
+            
+        #     if color == "white":
+        #         self.moves = ["up"]
+        #     else:
+        #         self.moves = ["down"]
+                
+        #     self.move_type = "finite"
+
+        # elif name == "rook":
+        #     print("Rook Created")
+        #     self.moves = ["up", "down", "left", "right"]
+        #     self.move_type = "infinite" 
+
+        # elif name == "bishop":
+        #     print("Bishop Created")
+        #     self.moves = "diagonal"
+        #     self.move_type = "infinite"
+
+        # elif name == "knight":
+        #     print("Knight Created")
+        #     self.moves = "hook"
+        #     self.move_type = "finite" 
+
+        # elif name == "queen":
+        #     print("Queen Created")
+        #     self.moves = ["up", "down", "left", "right", "diagonal"]
+        #     self.move_type = "infinite"
+
+        # else:
+        #     print("King Created")
+        #     self.moves = ["up", "down", "left", "right", "diagonal"]
+        #     self.move_type = "finite"
 
 class Square:
     def __init__(self, name, color,populated):
@@ -20,6 +57,7 @@ class Board:
 
         squares = []
         color = 'white'
+        
         # Loop through all possible squares 
         for rows in row:
             if color == "black":
@@ -77,11 +115,53 @@ class Board:
         black_queen_knight = Piece("knight", "black", "g1")    
         black_queen_rook = Piece("rook", "black", "h1")
 
-        self.pieces = [white_pawn_a, white_pawn_b, white_pawn_c, white_pawn_d, white_pawn_e, white_pawn_f, white_pawn_g, white_pawn_h, \
-            white_king_rook, white_king_knight, white_king_bishop, white_queen, white_king, white_queen_bishop, white_queen_knight, white_queen_rook, \
-            black_pawn_a, black_pawn_b, black_pawn_c, black_pawn_d, black_pawn_e, black_pawn_f, black_pawn_g, black_pawn_h, \
-            black_king_rook, black_king_knight, black_king_bishop, black_queen, black_king, black_queen_bishop, black_queen_knight, black_queen_rook            
-            ]
+        self.white_pieces = [white_pawn_a, white_pawn_b, white_pawn_c, white_pawn_d, white_pawn_e, white_pawn_f, white_pawn_g, white_pawn_h, \
+            white_king_rook, white_king_knight, white_king_bishop, white_queen, white_king, white_queen_bishop, white_queen_knight, white_queen_rook]
+        
+        self.black_pieces = [black_pawn_a, black_pawn_b, black_pawn_c, black_pawn_d, black_pawn_e, black_pawn_f, black_pawn_g, black_pawn_h, \
+            black_king_rook, black_king_knight, black_king_bishop, black_queen, black_king, black_queen_bishop, black_queen_knight, black_queen_rook]
+
+        self.round()
+
+    def round(self):
+        if self.color_turn == 'white':
+            row = np.asarray(["1", "2", "3", "4", "5", "6", "7", "8"])
+            col = np.asarray(["a", "b", "c", "d", "e", "f", "g", "h"])            
+            self.get_moves(self.white_pieces)
+        else:
+            row = np.asarray(["1", "2", "3", "4", "5", "6", "7", "8"])
+            col = np.asarray(["h", "g", "f", "e", "d", "c", "b", "a"]) 
+            self.get_moves(self.black_pieces)
+
+    def get_moves(self, pieces):
+
+        for piece in pieces:
+            piece_col_idx = np.where(col == piece.position[0])
+            piece_row_idx = np.where(row == piece.position[1])
+            
+            name = piece.name
+            color = piece.color 
+
+            moves = []
+            if name == "pawn":
+                if color == "white":
+                    pass
+
+            elif name == "rook":
+                pass 
+
+            elif name == "bishop":
+                pass
+
+            elif name == "knight":
+                pass
+
+            elif name == "queen":
+                pass
+            else:
+                pass               
 
 if __name__ == "__main__":
     chessboard = Board()
+
+    
